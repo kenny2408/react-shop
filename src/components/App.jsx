bd1d34b2af342287b09a768f9c737e8d209f3473
Kenny <kenny.mrsk@gmail.com>
2022-01-04 21:12:55 -0400

    04/01/2022

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hola mundo, bienvenidos al curso practico de React!!!</h1>
+    </div>
+  );
+};
+
+export default App;
